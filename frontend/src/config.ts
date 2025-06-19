a539fd8717f7d24a305acc183b4d708b7eb3e07d
miracchn <hannmiracc@gmail.com>
2025-06-19 19:37:49 +0300

    degisiklikler yapildi.

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'; 
+
+// Supabase Configuration
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://xmtuxxttnlrpvifutuwd.supabase.co';
+export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtdHV4eHR0bmxycHZpZnV0dXdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MDQ0NTMsImV4cCI6MjA1NzM4MDQ1M30.WSgkgqTt2uwk_de1joXJxdVQDYhxOHzlbHQvo7-nJas';
+
--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,8 +1,11 @@
 'use client';
 
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY} from '../config';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Environment değişkenleri yoksa varsayılan değerleri kullan
+const supabaseUrl = SUPABASE_URL
+const supabaseKey = SUPABASE_ANON_KEY
 
+// Supabase client'ı oluştur
 export const supabase = createClient(supabaseUrl, supabaseKey);
